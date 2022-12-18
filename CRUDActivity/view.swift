cc4a04a0ea49e1410bb2a9fb979c1815bfe25161
taif <toofy.556@hotmail.com>
2022-12-17 23:15:08 -0800

    111

---
+++ CRUDActivity/view.swift
@@ -0,0 +1,21 @@
+//
+//  view.swift
+//  CRUDActivity
+//
+//  Created by TAIF Al-zahrani on 23/05/1444 AH.
+//
+
+import UIKit
+import PencilKit
+
+class view: PKCanvasView{
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
